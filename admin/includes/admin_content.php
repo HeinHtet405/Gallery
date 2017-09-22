780f3d4381c56014408c75cd509421817c73d905
Hein Htet <heinhtet.hh405@gmail.com>
2017-09-22 09:57:35 +0630

    Again Create Project

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                ADMIN
+                <small>Subheading</small>
+            </h1>
+            
+            <?php 
+            
+//            $user = new User();
+//            $user->username = "Example_username";
+//            $user->password = "Example_password";
+//            $user->first_name = "John";
+//            $user->last_name = "Doe";
+//            
+//            $user->create();
+            
+//            $user = User::find_user_by_id(3);
+//            $user->last_name = "WILLIAMS";
+//            $user->update();
+            
+//             $user = User::find_user_by_id(3);
+//             $user->delete();
+            
+            
+            ?>
+        
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
